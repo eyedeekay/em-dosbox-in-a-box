60157f6fa210ccf8fae41284e45b5a3e95cf4009
idk <problemsolver@openmailbox.org>
2018-02-20 23:39:11 -0500

    doing it this way should work

--- launch.sh
+++ launch.sh
@@ -7,8 +7,9 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
     echo "mv $f" "$d"
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
     #./packager.py "$f" "$f" "$EXE"
-    ls *.html
     echo "./packager.py $c $c $e"
 done
+
+ls *.html
 
 darkhttpd /home/dosbox/em-dosbox/src/
