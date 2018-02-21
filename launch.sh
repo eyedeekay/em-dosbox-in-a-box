91750b2280131db2977d1593eef42b3a598e26c6
idk <problemsolver@openmailbox.org>
2018-02-21 01:12:00 -0500

    minor adjustments

--- launch.sh
+++ launch.sh
@@ -7,25 +7,33 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -mindepth 1 -typ
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
 
-    echo "Configure"
-    echo "========="
-    echo
-    echo "c=$c"
-    echo "d=$d"
-    echo "e=$e"
-    echo "f=$f"
-    echo
-    echo "moving files"
-    echo "------------"
-    echo "mv $f" "$d"
-    echo
-    echo "packaging files"
-    echo "---------------"
-    echo "./packager.py $c $c $e"
-    echo
-    mv "$f" "$d"
-    ./packager.py "$c" "$c" "$e"
+    echo "Configure" | tee -a log.md
+    echo "=========" | tee -a log.md
+    echo "" | tee -a log.md
+    echo " c=$c" | tee -a log.md
+    echo " d=$d" | tee -a log.md
+    echo " e=$e" | tee -a log.md
+    echo " f=$f" | tee -a log.md
+    echo "" | tee -a log.md
+    echo "###moving files" | tee -a log.md
+    echo "" | tee -a log.md
+    echo "  mv $f" "$d" | tee -a log.md
+    echo "" | tee -a log.md
+    echo "### packaging files" | tee -a log.md
+    echo "" | tee -a log.md
+    echo "  ./packager.py $c $c $e" | tee -a log.md
+    echo "" | tee -a log.md
+    echo "access game at:" | tee -a log.md
+    echo "---------------" | tee -a log.md
+    echo "" | tee -a log.md
+    echo "[$c.html]($c.html)" | tee -a log.md
+    echo "" | tee -a log.md
+
+    mv "$f" "$d" | tee -a log.md
+    ./packager.py "$c" "$c" "$e" | tee -a log.md
 done
+
+markdown log.md
 
 ls *.html
 
