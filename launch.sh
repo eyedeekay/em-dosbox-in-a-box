f164dbc162a0d54bd1618195ab356351b1d15e8c
idk <problemsolver@openmailbox.org>
2018-02-21 00:39:53 -0500

    minor adjustments

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 ls /home/dosbox/em-dosbox/src/programs/
 
-for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
+for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -mindepth 1 -type d ); do
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
