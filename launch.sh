4894ec1de67b96f42f1727e4a6771ddd2183fbe3
idk <problemsolver@openmailbox.org>
2018-02-20 23:13:42 -0500

    doing it this way should work

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
-for f in $(find programs -type d -d 1); do
-    mv "$f" /home/dosbox/em-dosbox/src/
+find --help
+for f in $(find /home/dosbox/em-dosbox/src/programs -type d -d 1); do
+    #mv "$f" $(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
+    echo "mv $f" $(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     EXE=$(find "$f" | grep -vi cwsdpmi | grep -i exe )
     #./packager.py "$f" "$f" "$EXE"
     find /home/dosbox/em-dosbox/src -type -d
