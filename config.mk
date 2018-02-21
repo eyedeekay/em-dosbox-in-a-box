22b3c1468121ea8213a71bccbdfb739533380a12
idk <problemsolver@openmailbox.org>
2018-02-20 23:53:33 -0500

    OK that launcher generates the pages list and such

--- config.mk
+++ config.mk
@@ -1,2 +1,3 @@
 
-MOUNT_PROGRAMS_FOLDER="--volume /media/media/Seagate Backup Plus Drive/DOS/the-eye.eu/public/Games/eXo/eXoDOS/Games:/home/dosbox/em-dosbox/src/programs"
+#MOUNT_PROGRAMS_FOLDER="--volume /media/media/Seagate Backup Plus Drive/DOS/the-eye.eu/public/Games/eXo/eXoDOS/Games:/home/dosbox/em-dosbox/src/programs"
+MOUNT_PROGRAMS_FOLDER="--volume programs:/home/dosbox/em-dosbox/src/programs"
