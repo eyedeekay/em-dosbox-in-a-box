52e8c2231a11fb5b5ef3e643ea2d2215209dc812
idk <problemsolver@openmailbox.org>
2018-02-20 23:32:26 -0500

    doing it this way should work

--- launch.sh
+++ launch.sh
@@ -2,11 +2,13 @@
 
 for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
     #mv "$f" $(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
-    echo "mv $f" $(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
-    EXE=$(find "$f" | grep -vi cwsdpmi | grep -i exe )
+    c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
+    d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
+    echo "mv $f" "$d"
+    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe )
     #./packager.py "$f" "$f" "$EXE"
     find /home/dosbox/em-dosbox/src -type -d
-    echo "./packager.py $f $f $EXE"
+    echo "./packager.py $c $c $e"
 done
 
 darkhttpd /home/dosbox/em-dosbox/src/
