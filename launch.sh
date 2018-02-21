e6362272411c246c42d3e97da588c9e70cd1507b
idk <problemsolver@openmailbox.org>
2018-02-21 13:50:43 -0500

    make it hash the files it's using. Need to find a gracenote-ish lookup service for dos game hashes, or build one.

--- launch.sh
+++ launch.sh
@@ -17,32 +17,43 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -mindepth 1 -typ
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
 
-    echo "" | tee -a log.md
-    echo "Configuring $c" | tee -a log.md
-    echo "--------------" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "  * c=$c" | tee -a log.md
-    echo "  * d=$d" | tee -a log.md
-    echo "  * e=$e" | tee -a log.md
-    echo "  * f=$f" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "#### moving files" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "  mv $f" "$d" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "#### packaging files" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "  ./packager.py $c $c $e" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "### access game at:" | tee -a log.md
-    echo "" | tee -a log.md
-    echo "[$c.html]($c.html)" | tee -a log.md
-    echo "" | tee -a log.md
+    echo "" | tee -a "$c.md"
+    echo "Configuring $c" | tee -a "$c.md"
+    echo "--------------" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    echo "  * c=$c" | tee -a "$c.md"
+    echo "  * d=$d" | tee -a "$c.md"
+    echo "  * e=$e" | tee -a "$c.md"
+    echo "  * f=$f" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    echo "#### linking files" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    ln -sf "$f" "$d" | tee -a "$c.md"
+    echo "  ln -sf $f" "$d" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    echo "#### hashing files" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    md5sum $d$e | tee -a "$c.md" | tee "$c.md5sum"
+    echo "  md5sum $d$e" | tee -a "$c.md"
+    sha1sum $d$e | tee -a "$c.md" | tee "$c.sha1sum"
+    echo "  sha1sum $d$e" | tee -a "$c.md"
+    sha256sum $d$e | tee -a "$c.md" | tee "$c.sha256sum"
+    echo "  sha256sum $d$e" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    echo "#### packaging files" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    ./packager.py "$c" "$c" "$e" | tee -a "$c.md"
+    echo "  ./packager.py $c $c $e" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    echo "### access game at:" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+    echo "[$c.html]($c.html)" | tee -a "$c.md"
+    echo "" | tee -a "$c.md"
+
+    cat "$c.md" | tee -a log.md
 
     markdown log.md | tee log.html
 
-    ln -sf "$f" "$d" | tee -a log.md
-    ./packager.py "$c" "$c" "$e" | tee -a log.md
 done
 
 markdown log.md | tee log.html
