0346965f90942fefc1bb507cd2095c394a54053b
idk <problemsolver@openmailbox.org>
2018-02-20 23:38:42 -0500

    doing it this way should work

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
-    #mv "$f" $(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
+    #mv "$f" "$d"
     echo "mv $f" "$d"
-    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c||g" )
+    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
     #./packager.py "$f" "$f" "$EXE"
     ls *.html
     echo "./packager.py $c $c $e"
