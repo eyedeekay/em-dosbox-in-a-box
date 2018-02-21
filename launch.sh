5aa7c4d7f7c4cfac45c8ee9827fff1dd9a20b58d
idk <problemsolver@openmailbox.org>
2018-02-20 23:40:35 -0500

    doing it this way should work

--- launch.sh
+++ launch.sh
@@ -3,11 +3,11 @@
 for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
-    #mv "$f" "$d"
+    mv "$f" "$d"
     echo "mv $f" "$d"
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
-    #./packager.py "$f" "$f" "$EXE"
-    echo "./packager.py $c $c $e"
+    ./packager.py "$f" "$f" "$EXE"
+    #echo "./packager.py $c $c $e"
 done
 
 ls *.html
