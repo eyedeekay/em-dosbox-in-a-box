a413620bf26c1bfb02122fa825b7191d034fd4b3
idk <problemsolver@openmailbox.org>
2018-02-20 23:35:18 -0500

    doing it this way should work

--- launch.sh
+++ launch.sh
@@ -5,9 +5,9 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     echo "mv $f" "$d"
-    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe )
+    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c||g" )
     #./packager.py "$f" "$f" "$EXE"
-    find /home/dosbox/em-dosbox/src -type -d
+    find /home/dosbox/em-dosbox/src -maxdepth 1 -type d
     echo "./packager.py $c $c $e"
 done
 
