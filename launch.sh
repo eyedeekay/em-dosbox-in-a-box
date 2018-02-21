9c1229aaaf3e2936e9ee86f6f1e3b2693e0c9fb3
idk <problemsolver@openmailbox.org>
2018-02-21 00:30:46 -0500

    minor adjustments

--- launch.sh
+++ launch.sh
@@ -5,13 +5,20 @@ ls /home/dosbox/em-dosbox/src/programs/*
 for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 0 -type d ); do
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
-    #mv "$f" "$d"
+    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
+
+    echo "$c $d $e"
+    echo "========="
     echo
+    echo "moving files"
+    echo "------------"
     echo "mv $f" "$d"
     echo
-    e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/||g" )
+    echo "packaging files"
+    echo "---------------"
     echo "./packager.py $c $c $e"
     echo
+    #mv "$f" "$d"
     #./packager.py "$c" "$c" "$e"
 done
 
