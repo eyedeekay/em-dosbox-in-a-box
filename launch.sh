665bd847055743addf7eb409380678f36fae43fb
idk <problemsolver@openmailbox.org>
2018-02-21 00:24:46 -0500

    OK that launcher generates the pages list and such

--- launch.sh
+++ launch.sh
@@ -6,6 +6,7 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     #mv "$f" "$d"
+    echo
     echo "mv $f" "$d"
     echo
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/||g" )
