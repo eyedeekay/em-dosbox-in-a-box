c4059e836fdfdb4311e1b48214d284e20209d7a8
idk <problemsolver@openmailbox.org>
2018-02-21 00:08:37 -0500

    OK that launcher generates the pages list and such

--- launch.sh
+++ launch.sh
@@ -4,10 +4,10 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -type d ); do
     c=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/||g')
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     mv "$f" "$d"
-    #echo "mv $f" "$d"
+    echo "mv $f" "$d"
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
     ./packager.py "$c" "$c" "$e"
-    #echo "./packager.py $c $c $e"
+    echo "./packager.py $c $c $e"
 done
 
 ls *.html
