f6a8876a40b44b286cff44d50ee41c31e6f8d215
idk <problemsolver@openmailbox.org>
2018-02-21 00:33:47 -0500

    minor adjustments

--- launch.sh
+++ launch.sh
@@ -7,8 +7,13 @@ for f in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 0 -type d ); do
     d=$(echo "$f" | sed 's|/home/dosbox/em-dosbox/src/programs/|/home/dosbox/em-dosbox/src/|g')
     e=$(find "$f" | grep -vi cwsdpmi | grep -i exe | sed "s|/home/dosbox/em-dosbox/src/programs/$c/||g" )
 
-    echo "$c $d $e"
+    echo "Configure"
     echo "========="
+    echo
+    echo "c=$c"
+    echo "d=$d"
+    echo "e=$e"
+    echo "f=$f"
     echo
     echo "moving files"
     echo "------------"
