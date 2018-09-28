ecb22b5d82a6ebc5af445e9564779adadd1fd873
idk <problemsolver@openmailbox.org>
2018-09-27 23:59:59 -0400

    fix index formatting

--- launch.sh
+++ launch.sh
@@ -20,48 +20,48 @@ for full_path in $(find /home/dosbox/em-dosbox/src/programs -maxdepth 1 -mindept
     echo "" | tee "$game_name.md"
     echo "Configuring $game_name" | tee -a "$game_name.md"
     echo "--------------" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "  * c=$game_name" | tee -a "$game_name.md"
     echo "  * d=$game_dir" | tee -a "$game_name.md"
     echo "  * e=$game_exe" | tee -a "$game_name.md"
     echo "  * f=$full_path" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "#### linking files" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     ln -sf "$full_path" "$game_dir" | tee -a "$game_name.md"
     echo "  ln -sf $full_path" "$game_dir" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "#### hashing files" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     md5sum "$game_dir/$game_exe" | tee -a "$game_name.md" | tee "$game_name.md5sum"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "  md5sum $game_dir/$game_exe" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     sha1sum "$game_dir/$game_exe" | tee -a "$game_name.md" | tee "$game_name.sha1sum"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "  sha1sum $game_dir/$game_exe" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     sha256sum "$game_dir/$game_exe" | tee -a "$game_name.md" | tee "$game_name.sha256sum"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "  sha256sum $game_dir/$game_exe" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "#### packaging files" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     ./packager.py "$game_name" "$game_name" "$game_exe" | tee -a "$game_name.md"
     echo "  ./packager.py $game_name $game_name $game_exe" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "### access game at:" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
     echo "[$game_name.html]($game_name.html)" | tee -a "$game_name.md"
-    echo "  " | tee -a "$game_name.md"
+    echo "" | tee -a "$game_name.md"
 
     log=$(cat log.md "$game_name.md")
-    echo $log | tee log.md
-    markdown "$game_name.md" | tee -a log.html
+    echo "$log" | tee log.md
+    pandoc "$game_name.md" -o log.html
 
 done
 
-markdown log.md | tee log.html
+pandoc log.md -o log.html
 
 killall darkhttpd; sleep 2
 darkhttpd /home/dosbox/em-dosbox/src/ --index log.html --log log.txt &
